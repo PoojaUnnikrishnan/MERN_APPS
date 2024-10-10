3d592ee5b0f8e512ac14d4f1c94b4103bbd01f53
Pooja Unnikrishnan <100412366+PoojaUnnikrishnan@users.noreply.github.com>
2024-10-10 15:34:59 +0530

    Update axios.js

--- Tinder/tinder_frontend/src/components/axios.js
+++ Tinder/tinder_frontend/src/components/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://tinder-web-app-backend.herokuapp.com/",
+baseURL:process.env.REACT_APP_BASE_URL,
 });
 export default instance;
